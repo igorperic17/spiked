ae7874b8f85ff5296e476f8d345f02acaf2ac675
Igor Peric <peric@Igors-MacBook-Pro.local>
2016-01-22 00:25:31 +0100

    test-stdp.py works, still has no STDP.

--- test-stdp.py
+++ test-stdp.py
@@ -1,5 +1,8 @@
 import pyNN.nest as sim
 import logging
+import pdb
+
+import matplotlib.pyplot as plt
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.StreamHandler())
@@ -7,16 +10,22 @@ logger.setLevel(logging.INFO)
 
 logger.info('Starting script.')
 
-sim.setup(timestep=0.1, min_delay=0.1, max_delay=1.0)
+sim.setup(timestep=0.1, min_delay=0.1)
 
-cell_params = {}
-pop1 = sim.Population(10, sim.IF_cond_alpha, cell_params)
+cell_params = { 'v_rest': -70.0, 'v_thresh': -52.0 }
+pop1 = sim.Population(10, sim.SpikeSourcePoisson, { 'rate' : 20.0 })
 pop2 = sim.Population(20, sim.IF_cond_alpha, cell_params)
 
-pop1.record_v(to_file=False)
-
+pop1.record('spikes', to_file=False)
 sim.run(1000)
 
 spikes = pop1.getSpikes()
+spikes = [int(x) for x in spikes.segments[0].spiketrains[0]] 
+# pdb.set_trace()
 
-print(spikes.)
+print('Recorded spikes:')
+print(spikes)
+x_ax = [1.0] * len(spikes)
+print(x_ax)
+plt.scatter(spikes, x_ax)
+plt.show()
